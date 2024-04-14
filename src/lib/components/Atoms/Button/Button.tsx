f999236ed7be3d8c8328e03b0059fea2b8e03521
Misq <michal.baran@live.com>
2024-04-14 18:43:46 +0200

    feat: init lib

---
+++ src/lib/components/Atoms/Button/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <button className=" rounded-xl p-12 text-red-700">Button LIBRARY</button>
+}
---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      include: './src/lib/**/*',
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  build: {
+    copyPublicDir: false,
+
+    lib: {
+      entry: resolve(__dirname, './src/lib/index.ts'),
+      name: 'react-beautiful-timeline',
+      fileName: (format) => `index.${format}.js`,
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          tailwindcss: 'tailwindcss',
+        },
+      },
+    },
+  },
+})
